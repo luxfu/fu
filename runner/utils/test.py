86edb5b67a698d8907529e66d3016cd2847dd777
luxfu <407197339@qq.com>
2025-07-16 17:42:35 +0800

    chore:测试文件

---
+++ runner/utils/test.py
@@ -0,0 +1,42 @@
+from functools import wraps
+
+
+def decorator1(func):
+    print("decorator1")
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("in decorator1")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def decorator2(func):
+    print("decorator2")
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("in decorator2")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def decorator3(func):
+    print("decorator3")
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("in decorator3")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@decorator1
+@decorator2
+@decorator3
+def addNum(x, y):
+    return x + y
+
+
+# if __name__ == '__main__':
+#     print(addNum(2, 3))
